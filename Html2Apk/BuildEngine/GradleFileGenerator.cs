8350bcad26d142432ec2a1b5b28b040b30acc3c0
Hecker <saadhasanusman@gmail.com>
2026-09-09 20:11:14 +0500

    chore: bump to Android 35 + build-tools 35.0.0

--- Html2Apk/BuildEngine/GradleFileGenerator.cs
+++ Html2Apk/BuildEngine/GradleFileGenerator.cs
@@ -23,12 +23,12 @@ public static class GradleFileGenerator
 apply plugin: 'com.android.application'
 
 android {{
-    compileSdk 34
+    compileSdk 35
     namespace '{cfg.PackageName}'
     defaultConfig {{
         applicationId '{cfg.PackageName}'
         minSdk 21
-        targetSdk 34
+        targetSdk 35
         versionCode 1
         versionName '1.0'
     }}
@@ -100,7 +100,7 @@ include ':app'
 
         return $@"<?xml version=""1.0"" encoding=""utf-8""?>
 <manifest xmlns:android=""http://schemas.android.com/apk/res/android"">
-    <uses-sdk android:minSdkVersion=""21"" android:targetSdkVersion=""34""/>
+    <uses-sdk android:minSdkVersion=""21"" android:targetSdkVersion=""35""/>
     {permXml}
     <application android:label=""{cfg.AppName}"" android:icon=""@mipmap/ic_launcher""
         android:theme=""@android:style/Theme.NoTitleBar.Fullscreen""
